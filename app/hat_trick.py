f302d6da114e2ddab5a3f581fd056d8bf843dd9c
BrokenShell <webmaster@sharpdesigndigital.com>
2022-04-11 07:54:32 -0700

    4.0.9

--- app/hat_trick.py
+++ app/hat_trick.py
@@ -10,36 +10,17 @@ def get_team_name(project: Project) -> str:
 
 def hat_trick(payload: Payload) -> Payload:
     random_project = TruffleShuffle(range(len(payload.projects)))
-    placed_learners = []
-    placed_learners.extend(map(lambda p: p.teamMemberSmtIds, payload.projects))
 
     for learner in payload.learners:
 
         if learner.labsProject != "":
-            # project_assignment = learner.labsProject
-            # project, *_ = filter(
-            #     lambda p: get_team_name(p) == project_assignment,
-            #     payload.projects,
-            # )
-            # if learner.lambdaId not in project.teamMemberSmtIds:
-            #     print("Missing Learner!")
-            #     print(f"Added {learner.name} to {project.product}")
-            #     project.teamMemberSmtIds.append(learner.lambdaId)
             continue
 
-        if learner.track == "Web":
-            small = min(len(project.teamMemberSmtIds) for project in payload.projects)
-            for project in payload.projects:
-                if len(project.teamMemberSmtIds) == small:
-                    project.teamMemberSmtIds.append(learner.lambdaId)
-                    learner.labsProject = get_team_name(project)
-                    break
-        else:
+        project_id = random_project()
+        while learner.track not in payload.projects[project_id].tracks:
             project_id = random_project()
-            while learner.track not in payload.projects[project_id].tracks:
-                project_id = random_project()
 
-            payload.projects[project_id].teamMemberSmtIds.append(learner.lambdaId)
-            learner.labsProject = get_team_name(payload.projects[project_id])
+        payload.projects[project_id].teamMemberSmtIds.append(learner.lambdaId)
+        learner.labsProject = get_team_name(payload.projects[project_id])
 
     return payload
