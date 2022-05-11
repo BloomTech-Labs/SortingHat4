4dfc466e7d08f7c945aea436a90035771866ff6f
BrokenShell <webmaster@sharpdesigndigital.com>
2022-05-11 13:34:37 -0700

    changes date format to iso standard

--- app/logger.py
+++ app/logger.py
@@ -60,5 +60,5 @@ class MongoDB:
 
 if __name__ == '__main__':
     db = MongoDB()
-    date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    date = datetime.datetime.now().isoformat()
     db.backup(f"backups/{date}.json")
