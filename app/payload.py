13e40f49f15786f347c5193cdecbbcb03c306424
BrokenShell <webmaster@sharpdesigndigital.com>
2022-05-11 13:13:55 -0700

    refactors example payload

--- app/payload.py
+++ app/payload.py
@@ -1,116 +1,52 @@
+from typing import List
+
 from numpy.random import randint
 
 from app.data_models import Payload, Learner, Project
 from app.generators import random_uuid, random_name
 
 
+def random_learner(track: str):
+    return Learner(
+        lambdaId=random_uuid(),
+        name=random_name(),
+        track=track,
+        tpmSkill1="B",
+        tpmSkill2="B",
+        tpmSkill3="C",
+        tpmInterest1=randint(1, 4),
+        tpmInterest2=randint(1, 4),
+        tpmInterest3=randint(1, 4),
+        tpmInterest4=randint(1, 4),
+        uxInterest1=randint(1, 4),
+        uxInterest2=randint(1, 4),
+        frontendInterest1=randint(1, 4),
+        frontendInterest2=randint(1, 4),
+        backendInterest1=randint(1, 4),
+        backendInterest2=randint(1, 4),
+    )
+
+
+def random_project(product: str, team_code: str, tracks: List[str]):
+    return Project(
+        id=f"{product} - {team_code}",
+        product=product,
+        teamCode=team_code,
+        releaseManager=random_name(),
+        tracks=tracks,
+        teamMemberSmtIds=[random_uuid() for _ in range(randint(0, 5))],
+    )
+
+
 example_payload = Payload(
     learners=[
-        Learner(
-            lambdaId=random_uuid(),
-            name=random_name(),
-            track="Web",
-            tpmSkill1="B",
-            tpmSkill2="B",
-            tpmSkill3="C",
-            tpmInterest1=randint(1, 4),
-            tpmInterest2=randint(1, 4),
-            tpmInterest3=randint(1, 4),
-            tpmInterest4=randint(1, 4),
-            uxInterest1=randint(1, 4),
-            uxInterest2=randint(1, 4),
-            frontendInterest1=randint(1, 4),
-            frontendInterest2=randint(1, 4),
-            backendInterest1=randint(1, 4),
-            backendInterest2=randint(1, 4),
-        ),
-        Learner(
-            lambdaId=random_uuid(),
-            name=random_name(),
-            track="Web",
-            tpmSkill1="B",
-            tpmSkill2="B",
-            tpmSkill3="C",
-            tpmInterest1=randint(1, 4),
-            tpmInterest2=randint(1, 4),
-            tpmInterest3=randint(1, 4),
-            tpmInterest4=randint(1, 4),
-            uxInterest1=randint(1, 4),
-            uxInterest2=randint(1, 4),
-            frontendInterest1=randint(1, 4),
-            frontendInterest2=randint(1, 4),
-            backendInterest1=randint(1, 4),
-            backendInterest2=randint(1, 4),
-        ),
-        Learner(
-            lambdaId=random_uuid(),
-            name=random_name(),
-            track="Web",
-            tpmSkill1="B",
-            tpmSkill2="B",
-            tpmSkill3="C",
-            tpmInterest1=randint(1, 4),
-            tpmInterest2=randint(1, 4),
-            tpmInterest3=randint(1, 4),
-            tpmInterest4=randint(1, 4),
-            uxInterest1=randint(1, 4),
-            uxInterest2=randint(1, 4),
-            frontendInterest1=randint(1, 4),
-            frontendInterest2=randint(1, 4),
-            backendInterest1=randint(1, 4),
-            backendInterest2=randint(1, 4),
-        ),
-        Learner(
-            lambdaId=random_uuid(),
-            name=random_name(),
-            track="DS",
-            tpmSkill1="B",
-            tpmSkill2="B",
-            tpmSkill3="C",
-            tpmInterest1=randint(1, 4),
-            tpmInterest2=randint(1, 4),
-            tpmInterest3=randint(1, 4),
-            tpmInterest4=randint(1, 4),
-            uxInterest1=randint(1, 4),
-            uxInterest2=randint(1, 4),
-            frontendInterest1=randint(1, 4),
-            frontendInterest2=randint(1, 4),
-            backendInterest1=randint(1, 4),
-            backendInterest2=randint(1, 4),
-        ),
+        random_learner("Web"),
+        random_learner("Web"),
+        random_learner("DS"),
     ],
     projects=[
-        Project(
-            id="Test Product - A",
-            product="Test Product",
-            teamCode="A",
-            releaseManager=random_name(),
-            tracks=["Web"],
-            teamMemberSmtIds=[random_uuid() for _ in range(randint(0, 5))],
-        ),
-        Project(
-            id="Test Product - B",
-            product="Test Product",
-            teamCode="B",
-            releaseManager=random_name(),
-            tracks=["Web"],
-            teamMemberSmtIds=[random_uuid() for _ in range(randint(0, 5))],
-        ),
-        Project(
-            id="Test Product - C",
-            product="Test Product",
-            teamCode="C",
-            releaseManager=random_name(),
-            tracks=["Web", "DS"],
-            teamMemberSmtIds=[random_uuid() for _ in range(randint(0, 5))],
-        ),
-        Project(
-            id="Test Product - D",
-            product="Test Product",
-            teamCode="D",
-            releaseManager=random_name(),
-            tracks=["Web"],
-            teamMemberSmtIds=[random_uuid() for _ in range(randint(0, 5))],
-        ),
+        random_project("Test Product", "A", ["Web"]),
+        random_project("Test Product", "B", ["Web"]),
+        random_project("Test Product", "C", ["Web", "DS"]),
     ],
 )
